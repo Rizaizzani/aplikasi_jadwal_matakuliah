34a68a7e2f13623554c91a6a27badff864d605cc
Rizaizzani <rizaizzani1227@gmail.com>
2025-01-09 20:37:47 +0700

    admin_screen.dart

--- lib/screens/admin_screen.dart
+++ lib/screens/admin_screen.dart
@@ -1,13 +1,75 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class AdminScreen extends StatelessWidget {
+
+class AdminScreen extends StatefulWidget {
   const AdminScreen({super.key});
+
+  @override
+  _AdminScreenState createState() => _AdminScreenState();
+}
+
+class _AdminScreenState extends State<AdminScreen> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final TextEditingController _phoneController = TextEditingController();
+  final TextEditingController _otpController = TextEditingController();
+  String? _verificationId;
+
+  Future<void> _sendCode() async {
+    try {
+      await _auth.verifyPhoneNumber(
+        phoneNumber: _phoneController.text,
+        verificationCompleted: (PhoneAuthCredential credential) async {
+          await _auth.signInWithCredential(credential);
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(builder: (context) => const ResultScreen()),
+          );
+        },
+        verificationFailed: (FirebaseAuthException e) {
+          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+            content: Text('Verification failed: ${e.message}'),
+          ));
+        },
+        codeSent: (String verificationId, int? resendToken) {
+          setState(() {
+            _verificationId = verificationId;
+          });
+        },
+        codeAutoRetrievalTimeout: (String verificationId) {},
+      );
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text('Error: $e'),
+      ));
+    }
+  }
+
+  Future<void> _verifyCode() async {
+    try {
+      final credential = PhoneAuthProvider.credential(
+        verificationId: _verificationId!,
+        smsCode: _otpController.text,
+      );
+      await _auth.signInWithCredential(credential);
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const ResultScreen()),
+      );
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: const Text('Masukkan OTP dengan benar'),
+        behavior: SnackBarBehavior.floating, // Memunculkan SnackBar di bagian bawah
+        margin: const EdgeInsets.all(16), // Menambahkan margin di sekitar SnackBar
+      ));
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Admin Screen'),
+        title: const Text('Admin Verification'),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
@@ -15,26 +77,20 @@ class AdminScreen extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             TextField(
+              controller: _phoneController,
               decoration: InputDecoration(
-                labelText: 'Enter Auth Number',
+                labelText: 'Enter Phone Number',
                 border: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(8),
                 ),
-                contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+                contentPadding:
+                const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
               ),
-              keyboardType: TextInputType.number,
-              onChanged: (value) {
-            ('Auth Number: $value');
-              },
+              keyboardType: TextInputType.phone,
             ),
             const SizedBox(height: 16),
             ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const ResultScreen()),
-                );
-              },
+              onPressed: _sendCode,
               style: ElevatedButton.styleFrom(
                 backgroundColor: Colors.lightBlue,
                 padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 16),
@@ -43,16 +99,57 @@ class AdminScreen extends StatelessWidget {
                 ),
               ),
               child: const Text(
-                'Authentication',
+                'Send OTP',
                 style: TextStyle(fontSize: 16, color: Colors.white),
               ),
             ),
+            const SizedBox(height: 16),
+            if (_verificationId != null) ...[
+              TextField(
+                controller: _otpController,
+                decoration: InputDecoration(
+                  labelText: 'Enter OTP',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  contentPadding:
+                  const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+                ),
+                keyboardType: TextInputType.number,
+              ),
+              const SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: _verifyCode,
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.green,
+                  padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 16),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                ),
+                child: const Text(
+                  'Verify OTP',
+                  style: TextStyle(fontSize: 16, color: Colors.white),
+                ),
+              ),
+            ],
           ],
         ),
       ),
     );
   }
 }
+
+
+
+
+
+
+
+
+
+
+
 
 class ResultScreen extends StatelessWidget {
   const ResultScreen({super.key});
@@ -61,18 +158,7 @@ class ResultScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Result'),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.logout),
-            onPressed: () {
-              Navigator.pushReplacement(
-                context,
-                MaterialPageRoute(builder: (context) => const LogoutScreen()),
-              );
-            },
-          ),
-        ],
+        title: const Text('Jadwal Kuliah'),
       ),
       body: Column(
         children: [
@@ -80,8 +166,9 @@ class ResultScreen extends StatelessWidget {
             child: Padding(
               padding: const EdgeInsets.all(16.0),
               child: ListView.builder(
-                itemCount: 3, // Sesuaikan jumlah item di sini
+                itemCount: 6, // Jumlah hari Senin hingga Sabtu
                 itemBuilder: (context, index) {
+                  List<String> hari = ['Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat', 'Sabtu'];
                   return Padding(
                     padding: const EdgeInsets.symmetric(vertical: 8.0),
                     child: Container(
@@ -90,19 +177,12 @@ class ResultScreen extends StatelessWidget {
                         borderRadius: BorderRadius.circular(8),
                       ),
                       child: ListTile(
-                        title: Text('Item ${index + 1}'),
-                        trailing: Row(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            IconButton(
-                              icon: const Icon(Icons.edit),
-                              onPressed: () {},
-                            ),
-                            IconButton(
-                              icon: const Icon(Icons.add),
-                              onPressed: () {},
-                            ),
-                          ],
+                        title: Text(hari[index]),
+                        trailing: IconButton(
+                          icon: const Icon(Icons.add),
+                          onPressed: () {
+                            _showAddScheduleModal(context, hari[index]);
+                          },
                         ),
                       ),
                     ),
@@ -111,42 +191,112 @@ class ResultScreen extends StatelessWidget {
               ),
             ),
           ),
-          const Padding(
-            padding: EdgeInsets.all(16.0),
-
-
-
-
-               child: null,
-              
-            ),
-
         ],
       ),
     );
   }
-}
 
-class LogoutScreen extends StatelessWidget {
-  const LogoutScreen({super.key});
+  void _showAddScheduleModal(BuildContext context, String hari) {
+    TextEditingController mataKuliahController = TextEditingController();
+    TextEditingController ruangController = TextEditingController();
+    TextEditingController waktuMulaiController = TextEditingController();
+    TextEditingController waktuSelesaiController = TextEditingController();
+    bool isNotifikasi = true;
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Logout'),
+    showModalBottomSheet(
+      context: context,
+      isScrollControlled: true,
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(top: Radius.circular(16)),
       ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pushReplacement(
-              context,
-              MaterialPageRoute(builder: (context) => const AdminScreen()),
-            );
-          },
-          child: const Text('Go Back to Admin Screen'),
-        ),
-      ),
+      builder: (BuildContext context) {
+        return Padding(
+          padding: EdgeInsets.only(
+            left: 16.0,
+            right: 16.0,
+            top: 16.0,
+            bottom: MediaQuery.of(context).viewInsets.bottom + 16.0,
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Tambah Jadwal - $hari',
+                style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 16),
+              TextField(
+                controller: mataKuliahController,
+                decoration: const InputDecoration(labelText: 'Mata Kuliah'),
+              ),
+              const SizedBox(height: 8),
+              TextField(
+                controller: ruangController,
+                decoration: const InputDecoration(labelText: 'Ruang'),
+              ),
+              const SizedBox(height: 8),
+              Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      controller: waktuMulaiController,
+                      decoration: const InputDecoration(labelText: 'Waktu Mulai'),
+                    ),
+                  ),
+                  const SizedBox(width: 8),
+                  Expanded(
+                    child: TextField(
+                      controller: waktuSelesaiController,
+                      decoration: const InputDecoration(labelText: 'Waktu Selesai'),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 16),
+              Row(
+                children: [
+                  Checkbox(
+                    value: isNotifikasi,
+                    onChanged: (value) {
+                      isNotifikasi = value ?? true;
+                    },
+                  ),
+                  const Text('Notifikasi'),
+                ],
+              ),
+              const SizedBox(height: 16),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TextButton(
+                    onPressed: () => Navigator.pop(context),
+                    child: const Text('BATAL'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      // Logika penyimpanan data
+                      print('Hari: $hari');
+                      print('Mata Kuliah: ${mataKuliahController.text}');
+                      print('Ruang: ${ruangController.text}');
+                      print('Waktu Mulai: ${waktuMulaiController.text}');
+                      print('Waktu Selesai: ${waktuSelesaiController.text}');
+                      print('Notifikasi: $isNotifikasi');
+                      Navigator.pop(context);
+                    },
+                    child: const Text('SIMPAN'),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        );
+      },
     );
   }
 }
+
+void main() {
+  runApp(const MaterialApp(home: ResultScreen()));
+}
+
